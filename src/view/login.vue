674d8e5453c91ae674bef50f04cc729eda4480b0
patrick4059 <huangwei4059@gmail.com>
2023-07-09 21:57:39 +0900

    新增代理jar

--- src/view/login.vue
+++ src/view/login.vue
@@ -32,101 +32,108 @@
 </template>
 
 <script>
-import { encrypt } from '@/utils/rsaEncrypt'
-import Config from '@/settings'
-import Cookies from 'js-cookie'
-import Background from '@/assets/images/background1.jpg'
+    import { encrypt } from '@/utils/rsaEncrypt'
+    import Config from '@/settings'
+    import Cookies from 'js-cookie'
+    import Background from '@/assets/images/background1.jpg'
 
-export default {
-  name: 'Login',
-  data () {
-    return {
-      Background: Background,
-      loginForm: {
-        username: 'admin',
-        password: '123456',
-        rememberMe: false,
-        code: '',
-        uuid: ''
-      },
-      loginRules: {
-        username: [{
-          required: true,
-          trigger: 'blur',
-          message: '用户名不能为空'
-        }],
-        password: [{
-          required: true,
-          trigger: 'blur',
-          message: '密码不能为空'
-        }],
-        code: [{
-          required: true,
-          trigger: 'change',
-          message: '验证码不能为空'
-        }]
-      },
-      loading: false
+    import axios from 'axios'
+
+    export default {
+        name: 'Login',
+        data() {
+            return {
+                Background: Background,
+                loginForm: {
+                    username: 'admin',
+                    password: '123456',
+                    rememberMe: false,
+                    code: '',
+                    uuid: ''
+                },
+                loginRules: {
+                    username: [{
+                        required: true,
+                        trigger: 'blur',
+                        message: '用户名不能为空'
+                    }],
+                    password: [{
+                        required: true,
+                        trigger: 'blur',
+                        message: '密码不能为空'
+                    }],
+                    code: [{
+                        required: true,
+                        trigger: 'change',
+                        message: '验证码不能为空'
+                    }]
+                },
+                loading: false
+            }
+        },
+        created() {
+            // 获取用户名密码等Cookie
+            this.getCookie()
+        },
+        methods: {
+            getCookie() {
+                const username = Cookies.get('username')
+                let password = Cookies.get('password')
+                const rememberMe = Cookies.get('rememberMe')
+                // 保存cookie里面的加密后的密码
+                this.cookiePass = password === undefined ? '' : password
+                password = password === undefined ? this.loginForm.password : password
+                this.loginForm = {
+                    username: username === undefined ? this.loginForm.username : username,
+                    password: password,
+                    rememberMe: rememberMe === undefined ? false : Boolean(rememberMe),
+                    code: ''
+                }
+            },
+            handleLogin() {
+                this.$refs.loginForm.validate(valid => {
+                    const user = {
+                        username: this.loginForm.username,
+                        password: this.loginForm.password,
+                        rememberMe: this.loginForm.rememberMe,
+                        code: this.loginForm.code,
+                        uuid: this.loginForm.uuid
+                    }
+                    if (user.password !== this.cookiePass) {
+                        user.password = encrypt(user.password)
+                    }
+                    if (valid) {
+                        this.loading = true
+                        if (user.rememberMe) {
+                            Cookies.set('username', user.username, { expires: Config.passCookieExpires })
+                            Cookies.set('password', user.password, { expires: Config.passCookieExpires })
+                            Cookies.set('rememberMe', user.rememberMe, { expires: Config.passCookieExpires })
+                        } else {
+                            Cookies.remove('username')
+                            Cookies.remove('password')
+                            Cookies.remove('rememberMe')
+                        }
+                        // this.$router.push({ path: this.redirect || '/dashboard' })
+                        //   this.$store.dispatch('Login', user)
+                        axios({
+                            url: 'http://localhost:8089/queryUsers',
+                            method: 'post'
+                        })
+                        // this.$store.dispatch('Login', user).then(() => {
+                        //   this.loading = false
+                        //   this.$router.push({ path: this.redirect || '/' })
+                        // }).catch(() => {
+                        //   this.loading = false
+                        //   this.getCode()
+                        // })
+                    } else {
+                        console.log('error submit!!')
+                        return false
+                    }
+                })
+            }
+        }
     }
-  },
-  created () {
-    // 获取用户名密码等Cookie
-    this.getCookie()
-  },
-  methods: {
-    getCookie() {
-      const username = Cookies.get('username')
-      let password = Cookies.get('password')
-      const rememberMe = Cookies.get('rememberMe')
-      // 保存cookie里面的加密后的密码
-      this.cookiePass = password === undefined ? '' : password
-      password = password === undefined ? this.loginForm.password : password
-      this.loginForm = {
-        username: username === undefined ? this.loginForm.username : username,
-        password: password,
-        rememberMe: rememberMe === undefined ? false : Boolean(rememberMe),
-        code: ''
-      }
-    },
-    handleLogin () {
-      this.$refs.loginForm.validate(valid => {
-        const user = {
-          username: this.loginForm.username,
-          password: this.loginForm.password,
-          rememberMe: this.loginForm.rememberMe,
-          code: this.loginForm.code,
-          uuid: this.loginForm.uuid
-        }
-        if (user.password !== this.cookiePass) {
-          user.password = encrypt(user.password)
-        }
-        if (valid) {
-          this.loading = true
-          if (user.rememberMe) {
-            Cookies.set('username', user.username, { expires: Config.passCookieExpires })
-            Cookies.set('password', user.password, { expires: Config.passCookieExpires })
-            Cookies.set('rememberMe', user.rememberMe, { expires: Config.passCookieExpires })
-          } else {
-            Cookies.remove('username')
-            Cookies.remove('password')
-            Cookies.remove('rememberMe')
-          }
-          this.$router.push({ path: this.redirect || '/dashboard' })
-          // this.$store.dispatch('Login', user).then(() => {
-          //   this.loading = false
-          //   this.$router.push({ path: this.redirect || '/' })
-          // }).catch(() => {
-          //   this.loading = false
-          //   this.getCode()
-          // })
-        } else {
-          console.log('error submit!!')
-          return false
-        }
-      })
-    }
-  }
-}
 </script>
 
 <style rel="stylesheet/scss" lang="scss">
