b3af58c36b65cce3e1e08b38095ab6d206e684c2
patrick4059 <huangwei4059@gmail.com>
2023-07-11 14:41:26 +0900

    登录增加security

--- src/utils/rsaEncrypt.js
+++ src/utils/rsaEncrypt.js
@@ -2,9 +2,7 @@ import JSEncrypt from 'jsencrypt/bin/jsencrypt.min'
 
 // 密钥对生成 http://web.chacuo.net/netrsakeypair
 
-const publicKey = 'MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBANL378k3RiZHWx5AfJqdH9xRNBmD9wGD\n' +
-  '2iRe41HdTNF8RUhNnHit5NpMNtGL0NPTSSpPjjI1kJfVorRvaQerUgkCAwEAAQ=='
-
+const publicKey = 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyyU1OwVjz1NsTgOD91KrM4O/ty27b2KI9k0+9LmnMjNtewnDCB1QkRyF8S8cQujXIRNHuBPqslOvoDgFeMDBPoFch0zyo+PfVduuJ0MkjsAxoQXuxbbvl+cNM4u3E+Xn1Qr/FrWK8sxp23aZw1D+jbA31+fwWzX75m1L0wSrhj0g/H2JY1R3zcuwupfjIyGF5qwnOE3SBbbuU22YtiZBJQZ5hwzv/pRdeFIe48XGL9l5C6L73Qi2qopwHM198RausUeLfcTk6xSjCH1MnTIaHvsAtO2fNjT+1Vq0hzINxnUWFJWw4NMTTSJ5220Oewb2Sz2IJTFNBKzOcezFeaCLeQIDAQAB'
 // 加密
 export function encrypt(txt) {
   const encryptor = new JSEncrypt()
